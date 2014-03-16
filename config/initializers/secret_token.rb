6194051d106edb175b3886cfa8057f5cc016f720
Max Norton <maxnorton.nz@gmail.com>
2014-03-16 09:54:20 +0000

    my first commit, new rails app and readme.md

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StrandedTravel::Application.config.secret_token = '07757d2d61ae7aa6d82ba5af0ae8093ed7e8d6640ad29cc8b724565d7fab91621fb2c2e2ad9a19d0217a36d8c18f7f02594fb6b42b426e9275c914c955ff39a3'
