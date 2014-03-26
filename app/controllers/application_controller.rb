3e65cd07a9db4bac4892969ca866d98da5bc107d
Max Norton <maxnorton.nz@gmail.com>
2014-03-26 10:59:20 +0000

    adding better exception handlers for cancan so better messages are displayed with appropriate redirrects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  rescue_from CanCan::AccessDenied do |exception|
+      # if authorize! :create, Destination, :alert => "Sorry you need to login to create destination." 
+      exception_actions = { 
+        index: 'view',
+        new: 'create a',
+        create: 'create a',
+        show: 'view that',
+        edit: 'edit that',
+        update: 'edit that',
+        destroy: 'delete that'
+       }
 
+      action_message = exception_actions.fetch(exception.action, 'access')
+      subject = exception.subject.class.to_s.downcase
+      subject = subject.pluralize if :index == exception.action
+
+      redirect_to main_app.root_url, :alert => "Sorry you do not have access to #{action_message} #{subject}."
+  end
   
 end
+
+
+  # authorize! :create, Destination, :message => "Sorry you need to login or create account first."
--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,6 +1,7 @@
 class DestinationsController < ApplicationController
   # before_filter :authenticate_user!  ##need this for omniauth?
   load_and_authorize_resource
+
   def index
     # # @destinations = Destination.all
     @q = Destination.search(params[:q])
